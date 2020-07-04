502d191468ca485e74a6f075cbd9d978fb5d8b1f
Michael Ciabuca <ciabuca@gmail.com>
2020-07-04 02:09:21 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -9,10 +9,8 @@
     [6, 4, 2]
   ]
 
-board = [" "," "," "," "," "," "," "," "," "]
-
-def display_board
-  puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+def display_board(board)
+  puts " #{board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
   puts "-----------"
