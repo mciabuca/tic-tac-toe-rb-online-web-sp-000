6fad5b8240509e69812a73c48a726fce8095c115
Michael Ciabuca <ciabuca@gmail.com>
2020-07-04 02:23:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,8 +21,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(board, index, current_player(board))
-  board[index] = current_player
+def move(board, index, current_player)
+  board[index] = current_player(board)
 end
 
 def position_taken?(board, index)
