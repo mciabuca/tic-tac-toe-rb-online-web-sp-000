8e9670ce2463e629c76f7007d0345a5d71a758be
Michael Ciabuca <ciabuca@gmail.com>
2020-07-04 02:14:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,4 +45,21 @@ def turn(board)
   end
 end
 
+def turn_count(board)
+  counter = 0
+  board.each do |turn|
+    if (turn == 'X' || turn == 'O')
+      counter += 1
+    end
+  end 
+  return counter
+end
+
+def current_player(board)
+ if(turn_count(board).even?)
+   return 'X'
+ else
+   return 'O'
+ end
+end
 
